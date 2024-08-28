ff92a87afc28de14a90fa169db4d44b68a58b25f
kybs00 <1016724958@qq.com>
2024-08-28 11:05:57 +0800

    添加项目文件。

---
+++ CameraCaptureDemo/MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System.Windows;
+using Windows.Devices.Enumeration;
+using Windows.Media.Capture;
+using Windows.UI.Xaml.Controls;
+using Button = Windows.UI.Xaml.Controls.Button;
+
+namespace CameraPreviewDemo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private CaptureElement _captureElement;
+        private MediaCapture _mediaCapture;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            AddUwpElement();
+        }
+        private void AddUwpElement()
+        {
+            Windows.UI.Xaml.Controls.Grid mainGrid = new Windows.UI.Xaml.Controls.Grid();
+            var captureElement = _captureElement = new CaptureElement()
+            {
+                Stretch = Windows.UI.Xaml.Media.Stretch.UniformToFill
+            };
+            mainGrid.Children.Add(captureElement);
+            var button = new Button()
+            {
+                VerticalAlignment = Windows.UI.Xaml.VerticalAlignment.Bottom,
+                HorizontalAlignment = Windows.UI.Xaml.HorizontalAlignment.Center,
+                Content = "关闭",
+                Width = 80
+            };
+            button.Click += CloseButton_Click;
+            mainGrid.Children.Add(button);
+            VideoViewHost.Child = mainGrid;
+        }
+
+        private void CloseButton_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            VideoViewHost.Visibility = Visibility.Collapsed;
+            _mediaCapture.StopPreviewAsync();
+        }
+
+        private async void TestButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            _mediaCapture = new MediaCapture();
+            var videos = await DeviceInformation.FindAllAsync(DeviceClass.VideoCapture);
+            var settings = new MediaCaptureInitializationSettings()
+            {
+                VideoDeviceId = videos[0].Id,
+                StreamingCaptureMode = StreamingCaptureMode.Video,
+            };
+            await _mediaCapture.InitializeAsync(settings);
+
+            VideoViewHost.Visibility = Visibility.Visible;
+            _captureElement.Source = _mediaCapture;
+            await _mediaCapture.StartPreviewAsync();
+        }
+    }
+}
